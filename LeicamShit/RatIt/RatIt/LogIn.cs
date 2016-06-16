ef53678ef9b11db1b70bb638a1843789100afb76
manuleicam <manu_scp@hotmail.com>
2016-06-16 13:46:09 +0100

    fdsdf

---
+++ LeicamShit/RatIt/RatIt/LogIn.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace RatIt
+{
+    class LogIn : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.LogIn);
+        }
+    }
---
+++ LeicamShit/RatIt/RatIt/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+
+namespace RatIt
+{
+    [Activity(Label = "RatIt", MainLauncher = true, Icon = "@drawable/mudardepois")]
+    public class MainActivity : Activity
+    {
+
+        private Button btnLogin;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            btnLogin = FindViewById<Button>(Resource.Id.imageButton1);
+
+            btnLogin.SetOnClickListener(this);
+        }
+
+        public void onClick(View v)
+        {
+            Intent intent = new Intent(this, LogIn.class);
+            startActivity(intent);
+        }
+    }
+}
